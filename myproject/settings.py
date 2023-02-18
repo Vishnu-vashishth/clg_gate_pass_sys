68dcac892f1226e0b6e2668c15314ce641b47d16
Vishnu-vashishth <vishnu.cse20@satyug.edu.in>
2023-02-18 09:58:10 +0530

    template path updated

--- myproject/settings.py
+++ myproject/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
