3087e2d018dd2759315f34f47442bdf70385cf26
Vishnu-vashishth <vishnu.cse20@satyug.edu.in>
2023-02-18 21:49:21 +0530

    updated approve request

--- myproject/views.py
+++ myproject/views.py
@@ -389,7 +389,7 @@ def approve_request(request,request_id):
             req.save()
             studentPhone = req.student.phone
             sendsms(f'+91{studentPhone}', f'Your request with id {req.request_id} has been approved by cc')
-            hodphone = teachers.objects.get(email = req.hod).phone
+            hodphone = teachers.objects.get(email = req.hod).first().phone
             sendsms(f'+91{hodphone}', f'Your request with id {req.request_id} has been approved by cc and is pending your approval. Please login to your account to approve the request.Follow the link https://clggatepasssys-production.up.railway.app/request_list ')
             return redirect('request_list')
 
