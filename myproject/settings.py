2f2e1781083c82cad43a11a5b98df8d69ebe6446
Vishnu-vashishth <vishnu.cse20@satyug.edu.in>
2023-02-18 08:52:09 +0530

    changes in vercel.json

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,8 +136,8 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles_build'/ 'static'
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
